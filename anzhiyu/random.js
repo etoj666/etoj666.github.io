4d1eb510d87043843a7cf0d4312319cf88bde913
CB_X2_Jun <zzh20120518@outlook.com>
2025-04-19 14:38:23 +0800

    Hexo

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/13/hello-world/","2025/04/13/paixusuanfashijianfuzadu/","2025/04/13/Luogu-problem-SP10509-solution/"];function toRandomPost(){
+var posts=["2025/04/13/Luogu-problem-SP10509-solution/","2025/04/13/paixusuanfashijianfuzadu/","2025/04/13/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
